4afd6628dcad78ec47de1fe3ccd12a6287a698f3
ljrrt <ljrrt.dev@gmail.com>
2023-10-25 20:35:22 +0200

    Authentificaction with google

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'dart:developer' as devtools show log;
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:ljrrt/state/auth/backend/authenticator.dart';
 
 import 'firebase_options.dart';
 
+//simplify print with log devtools
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
+
+// Run app after firebase init
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -11,6 +20,7 @@ void main() async {
   runApp(const App());
 }
 
+// Main app using MaterialApp
 class App extends StatelessWidget {
   const App({super.key});
 
@@ -25,7 +35,33 @@ class App extends StatelessWidget {
       ),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.dark,
-      home: const Placeholder(),
+      home: const LogIn(),
+    );
+  }
+}
+
+class LogIn extends StatelessWidget {
+  const LogIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign in'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          OutlinedButton(
+            onPressed: () async {
+              final result = await Authenticator().logInWithGoogle();
+              result.log();
+            },
+            child: const Text('Sign in with Google'),
+          ),
+        ],
+      )),
     );
   }
 }
